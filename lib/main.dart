f7c28181d280a53f13b38e9a4fc1efbdd9812ad3
Ghazi <ghazi@localhost.localdomain>
2019-02-10 14:37:22 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() async {
+
+  String _data = await getJson();
+
+  print(_data);
+  
+  runApp(new MaterialApp(
+    title: "JSON PArsing",
+    home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text("JSON Parse"),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: new Center(
+        child: new Text("Parsing..."),
+      ),
+    )
+  )
+  );   
+}
+
+Future<String> getJson() async {
+  String apiUrl = "https://jsonplaceholder.typicode.com/posts";
+  http.Response response = await http.get(apiUrl);
+
+  return json.decode(response.body).toString();
+}
